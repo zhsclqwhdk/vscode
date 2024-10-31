8889dbd61afa123cd42ccf699b4849e21cb99637
zhsclqwhdk <gkstkdgh17@naver.com>
2024-10-31 11:49:28 +0900

    Router 수정본

--- ex05/src/App.jsx
+++ ex05/src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Info from "./Info";
+import Header from "./Header";
 function App() {
   return (
     <>
       <h2>App.jsx</h2>
       <BrowserRouter>
         <Header />
-        <Link to="/">Home</Link>
-        <Link to="/info">Info</Link>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/info" element={<Info />} />
