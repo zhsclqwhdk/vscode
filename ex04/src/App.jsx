086ff95b75ca56bdb560feb1ee3f383328e132bd
zhsclqwhdk <gkstkdgh17@naver.com>
2024-10-31 10:21:05 +0900

    state 연습

--- ex04/src/App.jsx
+++ ex04/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //const [count, setCount] = useState(0); // useState는 초기값을 설정해줌
+  const [count, setCount] = useState(0);
+  const up = () => {
+    //alert("up 함수 호출!");
+    setCount(count + 1);
+  };
+  const down = () => {
+    //alert("down 함수 호출!");
+    setCount(count - 1);
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>count값:{count}</h2>
+      {/*클릭하면 count 값을 하나 올리는 Up 함수호출*/}
+      <button onClick={up}>Up</button>
+      <button onClick={down}>Down</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
