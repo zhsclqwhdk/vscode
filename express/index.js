cb4144487fe89879526efde45fed32cfab6b3756
zhsclqwhdk <gkstkdgh17@naver.com>
2024-11-05 11:00:59 +0900

    query string post 형식 및 json 이용

--- express/index.js
+++ express/index.js
@@ -3,6 +3,8 @@ import express from "express";
   /* 포트를 8000으로 설정하기 */
 }
 const app = express();
+// json 데이터를 주고받기위한 설정
+app.use(express.json());
 
 app.listen(8000, () => {
   console.log("서버 시작!");
@@ -29,4 +31,16 @@ app.get("/:id", (req) => {
   console.log(req.params);
   const { id } = req.params;
   console.log(id);
+});
+
+app.post("/hello", () => {
+  console.log("hello post 요청");
+});
+
+app.post("/post-req", (req) => {
+  console.log(req.body);
+  console.log(req.body.name);
+  console.log(req.body.age);
+  const { name, age } = req.body;
+  console.log(`name: ${name}, age: ${age}`);
 });
