6339ca3badf12f1e70c0bd1ee22d628f858bc5b3
zhsclqwhdk <gkstkdgh17@naver.com>
2024-11-05 10:44:00 +0900

    query string

--- express/index.js
+++ express/index.js
@@ -16,6 +16,14 @@ app.get("/hello", () => {
   console.log("hello 주소 요청");
 });
 
+app.get("/qs", (req) => {
+  console.log(req.query);
+  const { p1, p2 } = req.query;
+  // console.log 방식 2가지
+  console.log("p1: " + p1 + ", p2: " + p2);
+  console.log(`p1: ${p1}, p2: ${p2}`);
+});
+
 // board/10, product/6
 app.get("/:id", (req) => {
   console.log(req.params);
