6423bfe8642336933511a00b20984e923e1a5479
zhsclqwhdk <gkstkdgh17@naver.com>
2024-11-05 11:51:05 +0900

    db 설정

--- express/index.js
+++ express/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mysql from "mysql";
 {
   /* 포트를 8000으로 설정하기 */
 }
@@ -9,6 +10,20 @@ app.use(express.json());
 app.listen(8000, () => {
   console.log("서버 시작!");
 });
+/* createConnection 설정 */
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "user_ex",
+  password: "1234",
+  port: "3306",
+  database: "db_ex",
+});
+/* connect */
+db.connect((err) => {
+  console.log("db 연결 성공!");
+  console.log("err", err);
+});
+
 app.get("/", () => {
   console.log("기본주소 요청");
 });
