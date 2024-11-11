53166d4adfd57e8b0f0f32d786ba38526d101069
zhsclqwhdk <gkstkdgh17@naver.com>
2024-11-11 20:22:13 +0900

    test

--- member_project/frontend/src/App.jsx
+++ member_project/frontend/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
 
     return (
         <>
-            <div>test 113213412451525</div>
             <div>
                 <a href="https://vite.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
